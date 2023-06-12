03d68c97d27993ed0df34c340d514730951683a4
Sajid <asajid451@gmail.com>
2023-06-12 14:10:25 +0500

    Added basic structure

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,6 @@ const jwt = require("jsonwebtoken");
 
 // Assigning users to the variable User
 const User = db.user;
-console.log("🚀 ~ file: userController.js:8 ~ db:", db)
-console.log("🚀 ~ file: userController.js:8 ~ User:", User)
 
 const findAllUser = async (req, res) => {
   try {
