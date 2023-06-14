4a895def4573641f9423b637159bf7c2be613910
Sajid <asajid451@gmail.com>
2023-06-14 14:28:50 +0500

    code base chanes

--- models/retailerModel.js
+++ models/retailerModel.js
@@ -14,6 +14,10 @@ module.exports = (sequelize, DataTypes) => {
         isEmail: true, //checks for email format
         allowNull: true,
       },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       phone_number: {
         type: DataTypes.STRING,
         allowNull: true,
