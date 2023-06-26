aaf7e97211bb0911bb2f7c28dcb4e83ceaadadc6
Sajid <asajid451@gmail.com>
2023-06-26 11:10:30 +0500

    changes in API

--- Server.js
+++ Server.js
@@ -1,4 +1,5 @@
 //importing modules
+const cors = require("cors");
 const express = require("express");
 const sequelize = require("sequelize");
 const dotenv = require("dotenv").config();
@@ -22,6 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 //synchronizing the database and forcing it to false so we dont lose data
 db.sequelize.sync({ force: false }).then(() => {
